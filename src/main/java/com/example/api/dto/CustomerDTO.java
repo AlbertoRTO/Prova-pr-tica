a2165f9809b4ec14e668c24a4ec164ee76709cac
Alberto R Batista <alberto.batista@soulisto.com.br>
2020-10-17 15:30:03 -0300

    Implementando Security

---
+++ src/main/java/com/example/api/dto/CustomerDTO.java
@@ -0,0 +1,24 @@
+package com.example.api.dto;
+
+import com.example.api.domain.Customer;
+import lombok.Data;
+import org.modelmapper.ModelMapper;
+
+@Data
+public class CustomerDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+
+    /*public CustomerDTO(Customer customer) {
+        this.id = customer.getId();
+        this.nome = customer.getNome();
+        this.email = customer.getEmail();
+    }*/
+
+    public static CustomerDTO create(Customer customer) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(customer, CustomerDTO.class);
+    }
+}
